9b8cea7fa7bdc526909bc6512dfd16d993b84118
AKP-13 <alexpeirson@hotmail.com>
2020-08-20 11:58:31 +0100

    test setup

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -6,7 +6,7 @@ describe('App', () => {
         component = shallow(<App />);
     })
 
-    test("it renders", () => {
-        expect(component.find('#app')).toHaveLength(1);
+    test("has 4 routes", () => {
+        expect(component.find('Route')).toHaveLength(4);
     });
 })
